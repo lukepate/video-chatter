0d91a20b7a000eb319e5a6c7cfe20ca0b143b4f3
luke pate <lukewpate@gmail.com>
2017-09-13 22:15:40 -0400

    styling

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port 5000', app.settings.env);
 });
 
-// START SERVER
+// START SERVER 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
